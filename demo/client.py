112fb27388c1df429d892f84cf3e30a798880795
patrickpierson <patrick.c.pierson@gmail.com>
2016-07-03 19:35:35 -0400

    final demo stuff

--- demo/client.py
+++ demo/client.py
@@ -1,9 +1,17 @@
-# imports
+import boto3
 
-# when client runs query sqs for message
+sqs = boto3.resource('sqs')
 
-# get message
+queue = sqs.get_queue_by_name(QueueName='demo')
 
-# parse message for s3 location
-
-# display image url or display image via python ui?
+for message in queue.receive_messages(MessageAttributeNames=['Bucket']):
+  print message.body
+  print message.message_attributes.get('Bucket').get('StringValue')
+  s3 = boto3.resource('s3')
+  response = s3.Object(message.message_attributes.get('Bucket').get('StringValue'),
+                       message.body).get()["Body"].read()
+  #print response
+  message.delete()
+  fh = open(message.body, "wb")
+  fh.write(str(response))
+  fh.close()
--- demo/server.py
+++ demo/server.py
@@ -12,3 +12,16 @@ response = s3.Object('python-frederick-demo', filename).put(Body=open(filename, 
 print response
 
 os.remove(filename)
+
+sqs = boto3.resource('sqs')
+
+queue = sqs.get_queue_by_name(QueueName='demo')
+
+response = queue.send_message(MessageBody=filename, MessageAttributes={
+         'Bucket': {
+                     'StringValue': 'python-frederick-demo',
+                     'DataType': 'String'
+                 }})
+
+print('MessageId: ' + response.get('MessageId'))
+print('MD5: ' + response.get('MD5OfMessageBody'))
