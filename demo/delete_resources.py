86b0790f5f4ac095c06c8f01e635b7d4cd2b90cf
Patrick Pierson <patrickpierson@users.noreply.github.com>
2016-07-13 15:50:10 -0400

    edited out account number

--- demo/delete_resources.py
+++ demo/delete_resources.py
@@ -1,7 +1,7 @@
 import boto3
 
 sqs = boto3.client('sqs')
-response = sqs.delete_queue(QueueUrl='https://queue.amazonaws.com/464631411360/demo')
+response = sqs.delete_queue(QueueUrl='https://queue.amazonaws.com/########/demo')
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('python-frederick-demo')
